c524e2bce1ea3d3783517f535e6161dc77fe1f76
Maksim Kostromin <daggerok@gmail.com>
2019-05-28 01:12:47 +0300

    Implement ClassGraph library recurcive classes search.

---
+++ src/test/java/com/github/daggerok/ClassGraphLibraryTest.java
@@ -0,0 +1,28 @@
+package com.github.daggerok;
+
+import io.github.classgraph.ClassGraph;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@DisplayName("java classgraph library tests")
+public class ClassGraphLibraryTest {
+
+  @Test
+  @DisplayName("should get classes recursively in given package")
+  public void test() {
+    @Cleanup val scanResult = new ClassGraph().whitelistPackages(getClass().getPackage().getName())
+                                              .enableClassInfo()
+                                              .scan();
+    final List<Class<?>> classes = scanResult.getAllClasses().loadClasses();
+    assertThat(classes).hasSizeGreaterThan(4);
+    classes.stream().map(String::valueOf).forEach(log::info);
+  }
+}
